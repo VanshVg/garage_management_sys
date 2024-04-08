63d153b0d9a07a2e1a99086f5931e9f948aca69b
Jeel-patel007 <jeel.patel.2024.training@gmail.com>
2024-04-08 15:47:29 +0530

    modify:added constraint and default values to columns

--- database/garage_management_db.sql
+++ database/garage_management_db.sql
@@ -1,21 +1,21 @@
 CREATE TABLE payment_master(
-id INT PRIMARY KEY auto_increment,
-gst_amount DECIMAL(10,2),
-sub_total DECIMAL(10,2),
-discount_per DECIMAL(10,2),
-discount DECIMAL(10,2),
-appointment_id INT,
-FOREIGN KEY(appointment_id) REFERENCES appointments.id,
-status bool,
-type VARCHAR(255),
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+id INT PRIMARY KEY AUTO_INCREMENT,
+gst_amount DECIMAL(10,2) NOT NULL,
+sub_total DECIMAL(10,2) NOT NULL,
+discount_per DECIMAL(10,2) DEFAULT 0,
+discount DECIMAL(10,2) DEFAULT 0,
+appointment_id INT NOT NULL,
+status bool DEFAULT 0,
+type VARCHAR(255) NOT NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+FOREIGN KEY(appointment_id) REFERENCES appointments.id,);
 
 CREATE TABLE feedbacks(
-id INT PRIMARY KEY  auto_increment,
-garage_id INT,
+id INT PRIMARY KEY AUTO_INCREMENT,
+garage_id INT NOT NULL,
+customer_id INT NOT NULL,
+feedback VARCHAR(255) NOT NULL,
+ratings DECIMAL(10,2) DEFAULT 3,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 FOREIGN KEY(garage_id) REFERENCES garage_master.id,
-customer_id INT,
-FOREIGN KEY(customer_id) REFERENCES users.id,
-feedback VARCHAR(255),
-ratings DECIMAL(10,2),
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP);
+FOREIGN KEY(customer_id) REFERENCES users.id,);
