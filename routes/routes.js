cf49e19b5d224d1175f722cdff8c3d697b4e6e60
Bharat <makwana.bharat.2024@gmail.com>
2024-04-09 18:37:18 +0530

    fix:routing paths

--- routes/routes.js
+++ routes/routes.js
@@ -1,21 +1,24 @@
-import express from 'express';
-import { home } from '../controllers/staticControllers.js';
-import * as userController from '../controllers/userControllers.js';
-import { registerValidator, loginValidator } from '../validators/userValidation.js';
+import express from "express";
+import { home } from "../controllers/staticControllers.js";
+import * as userController from "../controllers/userControllers.js";
+import {
+  registerValidator,
+  loginValidator,
+} from "../validators/userValidation.js";
 // garage route file
-import * as garageRoutes from './garageRoutes.js';
+import * as garageRoutes from "./garageRoutes.js";
 
 const router = express.Router();
 
-router.get('/', userController.signUp);
-router.post('/register', userController.register);
-router.get('/signin', userController.signIn);
-router.post('/login', userController.login);
+router.get("/", userController.signUp);
+router.post("/register", userController.register);
+router.get("/signin", userController.signIn);
+router.post("/login", userController.login);
 
 // home page
-router.get('/home', home);
+router.get("/home", home);
 
-// garage routes 
-router.use('/garage', garageRoutes);
+// garage routes
+// router.use('/garage', garageRoutes);
 
 export default router;
