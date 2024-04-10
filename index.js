92259c8a2869086740f7d3283f1df97aa3a0c81b
Jeel-patel007 <jeel.patel.2024.training@gmail.com>
2024-04-10 13:30:14 +0530

    modify:api for insertion of garagemodule, added:helpers folder and file updload function for thumbnail of garage

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ config();
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 const port = process.env.PORT || 3000;
--- routes/routes.js
+++ routes/routes.js
@@ -6,27 +6,20 @@ import {
   loginValidator,
 } from "../validators/userValidation.js";
 // garage route file
-import * as garageRoutes from "./garageRoutes.js";
+import garageRoutes from "./garageRoutes.js";
 
 const router = express.Router();
 
-<<<<<<< HEAD
 // auth routes
 router.get('/', userController.signUp);
 router.post('/register', registerValidator, userController.register);
 router.get('/signin', userController.signIn);
 router.post('/login', loginValidator, userController.login);
-=======
-router.get("/", userController.signUp);
-router.post("/register", userController.register);
-router.get("/signin", userController.signIn);
-router.post("/login", userController.login);
->>>>>>> b5eaeec8e3616f57952efc686a851c2b5151713d
 
 // home page
 router.get("/home", home);
 
 // garage routes
-// router.use('/garage', garageRoutes);
+router.use('/garage', garageRoutes);
 
 export default router;
