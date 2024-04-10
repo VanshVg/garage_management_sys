4d32827a5c6a17c7386b773d401633a47189e399
Bharat <makwana.bharat.2024@gmail.com>
2024-04-10 19:02:20 +0530

    improve:Dashboard Design & Registe Screen

--- routes/routes.js
+++ routes/routes.js
@@ -1,19 +1,23 @@
-import express from 'express';
-import { home, userProfile } from '../controllers/staticControllers.js';
+import express from "express";
+import { home, userProfile } from "../controllers/staticControllers.js";
 import * as userController from "../controllers/userControllers.js";
 import garageRoutes from "./garageRoutes.js";
-import passport from 'passport';
+import passport from "passport";
 import {
   registerValidator,
   loginValidator,
 } from "../validators/userValidation.js";
-import { slotValidator } from '../validators/slotValidation.js';
-import { slotBooking, slotDelete, slotUpdate } from '../controllers/slotBookingController.js';
+import { slotValidator } from "../validators/slotValidation.js";
+import {
+  slotBooking,
+  slotDelete,
+  slotUpdate,
+} from "../controllers/slotBookingController.js";
 
 // garage route file
-import profileRoutes from "./profileRoutes.js"
-import { applyPassportStrategy } from '../auth/auth.js';
-import { validateRole } from '../roleServices.js';
+import profileRoutes from "./profileRoutes.js";
+import { applyPassportStrategy } from "../auth/auth.js";
+import { validateRole } from "../roleServices.js";
 
 const router = express.Router();
 applyPassportStrategy();
@@ -27,17 +31,26 @@ router.get("/activate/:id/:token", userController.activate);
 router.get("/home", home);
 
 // garage routes
-router.use('/garage', garageRoutes);
+router.use("/garage", garageRoutes);
 
-router.get('/profile', passport.authenticate("jwt", {
-  session: false,
-  failureRedirect: "/signIn"
-}), validateRole(1), userProfile);
+router.get(
+  "/profile",
+  passport.authenticate("jwt", {
+    session: false,
+    failureRedirect: "/signIn",
+  }),
+  validateRole(1),
+  userProfile
+);
 
-router.use("/profile", profileRoutes)
+router.use("/profile", profileRoutes);
 
 // slot routes
-router.post('/slotinsert',slotBooking)
-router.post('/slotupdate',slotUpdate)
-router.post('/slotdelete',slotDelete)
+router.post("/slotinsert", slotBooking);
+router.post("/slotupdate", slotUpdate);
+router.post("/slotdelete", slotDelete);
+
+router.all("*", (req, res) => {
+  res.send("404");
+});
 export default router;
