9e720161a6f3a7df05076233e2a4a04a25d56343
shaileshbhai24 <shaileshbhai.chaudhari.2024@gmail.com>
2024-04-10 14:39:46 +0530

    improve: role based access using passport-jwt

--- routes/routes.js
+++ routes/routes.js
@@ -2,15 +2,16 @@ import express from 'express';
 import { home, userProfile } from '../controllers/staticControllers.js';
 import * as userController from "../controllers/userControllers.js";
 import * as garageRoutes from "./garageRoutes.js";
+import passport from 'passport';
 import {
   registerValidator,
   loginValidator,
 } from "../validators/userValidation.js";
-// garage route file
-
+import { applyPassportStrategy } from '../auth/auth.js';
+import { validateRole } from '../roleServices.js';
 
 const router = express.Router();
-
+applyPassportStrategy();
 // auth routes
 router.get('/', userController.signUp);
 router.post('/register', registerValidator, userController.register);
@@ -23,6 +24,9 @@ router.get("/home", home);
 // garage routes
 // router.use('/garage', garageRoutes);
 
-router.get('/profile', userProfile);
+router.get('/profile', passport.authenticate("jwt", {
+  session: false,
+  failureRedirect: "/signIn"
+}), validateRole(1), userProfile);
 
 export default router;
