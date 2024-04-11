ebfb9ca9129d4563f4801b4038ecc1704a297353
Jeel-patel007 <jeel.patel.2024.training@gmail.com>
2024-04-11 17:59:08 +0530

    modified:garageform style and intigrate insert and update api with garage ui

--- public/scripts/garageForm.js
+++ public/scripts/garageForm.js
@@ -14,3 +14,51 @@ const prevPage = () => {
   tabs[1].style.display = 'none';
 }
 showTabs(currentTab);
+// taking form and request of insert
+const addGarageDetails = async () => {
+
+  let formData = document.getElementById("garageAdd");
+  let details = new FormData(formData);
+  const params = new URLSearchParams(details);
+  const garageData = await new Response(params).text();
+  console.log(garageData);
+  let data = await fetch("http://localhost:3000/garage/add", {
+    method: "POST",
+    headers: {
+      'Content-type': 'application/x-www-form-urlencoded'
+    },
+    body: garageData
+
+  })
+  let result = await data.json();
+  document.getElementById("message").innerText = result.message;
+  console.log(result);
+}
+
+const updateGarage = async () => {
+  let formData = document.getElementById("garageAdd");
+  let details = new FormData(formData);
+  const params = new URLSearchParams(details);
+  const garageData = await new Response(params).text();
+  console.log(garageData);
+  let data = await fetch("http://localhost:3000/garage/update", {
+    method: "POST",
+    headers: {
+      'Content-type': 'application/x-www-form-urlencoded'
+    },
+    body: garageData
+
+  })
+  let result = await data.json();
+  document.getElementById("message").innerText = result.message;
+  console.log(result);
+}
+
+const handleGarage = () => {
+  if (window.location.href == 'http://localhost:3000/garage/garageUpdate') {
+    updateGarage();
+  }
+  else {
+    addGarageDetails();
+  }
+}
