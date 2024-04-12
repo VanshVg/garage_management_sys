a916cf042db6e61716ad94c4e5e28f28457b22c7
VanshVg17 <vansh.gandhi.2024@gmail.com>
2024-04-12 16:46:16 +0530

    added: integrated backend api in add service page

--- public/scripts/service.js
+++ public/scripts/service.js
@@ -1,15 +1,41 @@
-let showServices = () => {
-  console.log("Inside");
+const showServices = () => {
   let services = ["Service1", "Service2", "Service3", "Service4"];
   let select = document.createElement("select");
+  select.id = "service-name";
   services.forEach((element) => {
     let option = document.createElement("option");
     option.value = element;
     option.name = element;
-    option.textContent = element
+    option.textContent = element;
     select.appendChild(option);
-  })
+  });
   document.getElementsByClassName("select-service")[0].appendChild(select);
-}
+};
+
+const addService = async () => {
+  let value = document.getElementById("service-name").value;
+  let body = {
+    name: value,
+  };
+
+  let data = await fetch(`/owner/service/1`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  });
+  let response = await data.json();
+  if(response.success==true) {
+    window.location.href = "/owner/home"
+  } else {
+    if(document.getElementById("error")) {
+      document.getElementById("error").remove()
+    }
+    let error = document.createElement("p");
+    error.id = "error";
+    error.style.color = "red";
+    error.textContent = response.message;
+    document.getElementsByClassName("service-top")[0].appendChild(error)
+  }
+};
 
 showServices();
