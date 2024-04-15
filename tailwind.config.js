28d45cbfcec37ebd94098600d9c0c1dcc1ba2f9d
VanshVg17 <vansh.gandhi.2024@gmail.com>
2024-04-15 19:11:53 +0530

    added: created add vehicle page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./views/**/*.{ejs,js}"],
   theme: {
     extend: {},
+    colors: {
+      dark: "#152533",
+      lightBlue: "#D9D9D9",
+      blue: "#355364",
+      white: "#FFFFFF",
+      light: "#D9D9D9",
+      lightbg: "#d7e0e7",
+      lightorange: "burlywood",
+      card1bg: "rgba(255, 225, 200, 0.5)",
+      card1text: "#e96a00",
+      card2bg: "rgba(225, 246, 247, 0.5)",
+      card2text: "#34cdd0",
+      card3bg: "rgba(252, 226, 225, 0.5)",
+      card3text: "#df5e5e",
+      card4bg: "rgba(237, 231, 251, 0.5)",
+      card4text: "#9a78ea",
+    },
   },
   plugins: [],
-}
-
+};
