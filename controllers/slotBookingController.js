86bba385b24b5c3dafde92fe0ef2756d4ee1702b
ganpat parmar <ganpat5489@gmail.com>
2024-04-19 18:35:49 +0530

    modified all pagination and filetring at owner side

--- controllers/slotBookingController.js
+++ controllers/slotBookingController.js
@@ -1,5 +1,5 @@
 import { validationResult } from "express-validator";
-import { deleteSlot, getAllSlots, insertSlot, updateSlot } from "../utils/dbHandler.js";
+import { deleteSlot, getAllSlots, insertSlot, updateSlot,findOne } from "../utils/dbHandler.js";
 
 
 export const slotBooking = async (req, res) => {
@@ -49,7 +49,11 @@ export const slotDelete = async (req, res) => {
 
 export const getSlots = async (req, res) => {
     const { startIndex, endIndex } = req.pagination
-    const result = await getAllSlots(startIndex)
+    const garage = req.query.garage
+    const user = req.user.email
+    const userExist = await findOne(user)
+    const result = await getAllSlots(startIndex,garage,userExist[0].id)
+    console.log(result);
     const totalPage = Math.ceil(result[1][0].count / 10)
     res.json({ result: result[0], count: result[1][0].count, startIndex: startIndex, endIndex: endIndex, totalPage: totalPage })
 
