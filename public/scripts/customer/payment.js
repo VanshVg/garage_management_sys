e90f7a82d4372674bc229764e8c673cbd8be3867
Bharat <makwana.bharat.2024@gmail.com>
2024-05-02 14:23:40 +0530

    fix: step validation and localstroage issue

--- public/scripts/customer/payment.js
+++ public/scripts/customer/payment.js
@@ -208,3 +208,142 @@ const addPayment = async (finalAmount) => {
     });
   }
 };
+
+//////
+//  Swal.fire({
+//    width: "800px",
+//    showConfirmButton: false,
+//    html: `
+//         <div class="mx-auto px-4" id="payment-container">
+//       <h1 class="text-center text-dark text-3xl font-bold">Payment Details</h1>
+//       <p class="text-center mt-5 text-lg text-dark">
+//         Payment of : Slot Booking Charges (₹ 20 /-)
+//       </p>
+//       <div class="mx-auto mt-10 bg-white rounded-md py-10">
+//         <div class="flex mx-auto justify-between">
+//           <label class="text-dark text-lg font-semibold  text-left">Choose a payment type:</label>
+//           <select
+//             class="rounded-lg  px-2 py-2 h-[40px] my-auto text-dark w-3/5 text-sm bg-lightbg"
+//             id="payment-type"
+//             onchange="changePayment()"
+//             name="paymentType"
+//           >
+//             <option value="card">Debit Card/Credit Card</option>
+//             <option value="upi">UPI</option>
+//             <option value="net banking">Net Banking</option>
+//             <option value="cash">Cash</option>
+//           </select>
+//         </div>
+//         <div class="flex justify-between mt-5" id="bank-section">
+//           <label class="text-dark text-lg font-semibold  text-left">Choose your bank: </label>
+//           <select
+//             class="rounded-lg  px-2 py-2 h-[40px] my-auto text-dark w-3/5 text-sm bg-lightbg"
+//             name="bankName"
+//           >
+//             <option value="sbi">SBI Bank</option>
+//             <option value="hdfc">HDFC Bank</option>
+//             <option value="bob">Bank of Baroda</option>
+//             <option value="icici">ICICI Bank</option>
+//             <option value="axis">Axis Bank</option>
+//           </select>
+//         </div>
+//         <div
+//           class="bg-dark h-[1px] w-[700px] mx-auto mt-10"
+//           id="payment-line"
+//         ></div>
+//         <div class="w-full" id="card-section">
+//           <h2 class="text-2xl text-dark font-bold">Card Details</h2>
+//           <div class="flex justify-center mt-5">
+//             <label class="text-dark text-lg">Card Number: </label>
+//             <input
+//               class="ml-[72px] rounded h-[30px] bg-lightbg placeholder:text-dark placeholder:opacity-70 px-2"
+//               name="cardNumber"
+//               type="text"
+//               placeholder="Enter card number"
+//             />
+//           </div>
+//           <div class="flex justify-center mt-5">
+//             <label class="text-dark text-lg">Account holder name: </label>
+//             <input
+//               class="ml-2 rounded h-[30px] bg-lightbg placeholder:text-dark placeholder:opacity-70 px-2"
+//               name="accountHolder"
+//               type="text"
+//               placeholder="Enter card holder name"
+//             />
+//           </div>
+//           <div class="flex justify-center">
+//             <div class="flex justify-center mt-5">
+//               <label class="text-dark text-lg">CVV: </label>
+//               <input
+//                 class="ml-2 rounded h-[30px] w-[50px] bg-lightbg placeholder:text-2xl placeholder:text-dark placeholder:opacity-60 px-2"
+//                 name="cvv"
+//                 type="text"
+//                 maxlength="3"
+//                 placeholder="- - -"
+//               />
+//             </div>
+//             <div class="flex justify-center mt-5 ml-[85px]">
+//               <label class="text-dark text-lg">Expiry Date: </label>
+//               <input
+//                 class="ml-[35px] rounded h-[30px] w-[100px] bg-lightbg placeholder:text-dark placeholder:opacity-60 px-2"
+//                 name="expiryDate"
+//                 type="text"
+//                 placeholder="MM-YYYY"
+//               />
+//             </div>
+//           </div>
+//         </div>
+//         <div class="mt-5 hidden" id="upi-section">
+//           <h2 class="text-center text-2xl text-dark">UPI Details</h2>
+//           <div class="flex justify-center mt-5">
+//             <label class="text-dark text-lg">UPI Id: </label>
+//             <input
+//               class="ml-2 rounded h-[30px] bg-lightbg placeholder:text-dark placeholder:opacity-70 px-2"
+//               name="upi"
+//               type="text"
+//               placeholder="Enter your UPI id"
+//             />
+//           </div>
+//         </div>
+//         <div class="mt-10">
+//           <p
+//             class="text-center text-white mx-auto pt-[13px] rounded bg-dark w-[120px] h-[50px] hover:cursor-pointer"
+//           >
+//             Pay Now
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//   `,
+//  }).then((result) => {
+//    if (result.isConfirmed) {
+//      let timerInterval;
+//      Swal.fire({
+//        title: "Your Payment is in Preogress..!!",
+//        timer: 2000,
+//        timerProgressBar: true,
+//        didOpen: () => {
+//          Swal.showLoading();
+//        },
+//        willClose: () => {
+//          clearInterval(timerInterval);
+//        },
+//      }).then((result) => {
+//        Swal.fire({
+//          position: "top-end",
+//          icon: "success",
+//          title: "Payment Done..",
+//          showConfirmButton: false,
+//          timer: 1500,
+//          didOpen: () => {
+//            // Swal.showLoading();
+//          },
+//          willClose: () => {
+//            localStorage.clear();
+//            // location.href = "/customer/profile";
+//            return;
+//          },
+//        });
+//      });
+//    }
+//  });
--- public/scripts/customer/stepHandler.js
+++ public/scripts/customer/stepHandler.js
@@ -1,4 +1,7 @@
-const home = () => (location.href = "/");
+const home = () => {
+  location.href = "/";
+  localStorage.clear();
+};
 class validateStore {
   static garage() {
     return true;
@@ -32,7 +35,7 @@ class validateStore {
   static slots() {
     validateStore.service();
     let id = localStorage.getItem("serviceId");
-    console.log(isNaN(parseInt(id)));
+    console.log(parseInt(id));
     if (isNaN(parseInt(id))) {
       localStorage.setItem("index", 3);
       setActive(screen[3]);
@@ -48,18 +51,9 @@ class validateStore {
       return false;
     } else return true;
   }
-  static book() {
-    if (validateStore.payment()) {
-      book();
-    }
-    // if (!localStorage.getItem("paymentId")) {
-    // location.href = "/customer/payment";
-    // } else {
-    // let formPlace = document.getElementById("other");
-    // formPlace.style.display = "none";
-    // formPlace.style.zIndex = 0;
-    // }
-    return false;
+  static profile() {
+    localStorage.clear();
+    return true;
   }
 }
 class storeHandler {
@@ -71,14 +65,26 @@ class storeHandler {
     storeHandler.store("garageId", id);
   }
   static vehicleTypeSelection() {
+    if (!validateStore.garage() || localStorage.getItem("index") != "1") {
+      localStorage.clear();
+      return;
+    }
     let id = document.querySelector("input[name=type]:checked")?.value;
     storeHandler.store("typeId", id);
   }
   static vehicleSelection() {
+    if (!validateStore.type()) {
+      localStorage.clear();
+      return;
+    }
     let id = document.querySelector("input[name=vehicle]:checked")?.value;
     storeHandler.store("vehicleId", id);
   }
   static serviceSelection() {
+    if (!validateStore.vehicle()) {
+      localStorage.clear();
+      return;
+    }
     let selectedServices = [];
     document
       .querySelectorAll("input[name=service]:checked")
@@ -88,10 +94,18 @@ class storeHandler {
     storeHandler.store("serviceId", selectedServices);
   }
   static slotSelection() {
+    if (!validateStore.service()) {
+      localStorage.clear();
+      return;
+    }
     let id = document.querySelector("input[name=slots]:checked")?.value;
     storeHandler.store("slotId", id);
   }
   static paymentSelection() {
+    if (!validateStore.slots()) {
+      localStorage.clear();
+      return;
+    }
     let id = document.querySelector("input[name=payment]:checked")?.value;
     storeHandler.store("paymentId", id);
   }
@@ -122,9 +136,9 @@ class htmlHandler {
     eventListenter
   ) {
     if (!validateStore[step]()) {
-      console.log(step);
       return;
     }
+    console.log(step);
     let data = await APICaller.callAPIHandler(step, params);
     let html = htmlHandler.fillHtml(data, errorMessage, step, stepTitle);
     document.getElementById(`${container}-container`).innerHTML = html;
@@ -140,8 +154,9 @@ class htmlHandler {
     let html = `
     <div class="h-full w-full p-3">
         <strong class="text-white text-left">${Title}</strong><hr/>
-        <div class="max-h-[95%] h-max w-full overflow-scroll flex  ${stepHTML == "type" ? "flex-row flex-wrap" : "flex-col"
-      } ">
+        <div class="max-h-[95%] h-max w-full overflow-scroll flex  ${
+          stepHTML == "type" ? "flex-row flex-wrap" : "flex-col"
+        } ">
     `;
     if (!data.result.length) {
       html += htmlHandler.emptyHandler(message);
@@ -165,8 +180,9 @@ class htmlHandler {
       address =
         address.length > 50 ? address.substring(0, 50) + "..." : address;
       garageList += `
-                        <input type='radio' id='garage-${data.id
-        }' name='garage' value='${data.id}'/>
+                        <input type='radio' id='garage-${
+                          data.id
+                        }' name='garage' value='${data.id}'/>
                         <label for="garage-${data.id}" class="cursor-pointer">
                         <div class="relative bg-[rgba(0,0,0,.2)] p-2 w-full h-[100px] mt-5 rounded-lg flex" style="box-shadow:1px 1px 1px rgba(0,0,0,.2),inset 1px 1px 1px rgba(255,255,255,.2)">
                       <div class="absolute top-0.5 right-0.5 bg-yellow-600 flex rounded-md px-1 justify-center items-center">
@@ -174,14 +190,16 @@ class htmlHandler {
                         <span class="text-[12px] text-white text-semibold mx-0.5">5.0</span>
                         </div>
                             <div class="garage-icon border-2 w-2/4 rounded-md overflow-hidden">
-                          <img src="/uploads/${data.thumbnail
-        }" onerror="this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeUyb754vebKqfbxScXd11wIOQGyxRlNNQBv31JG4wC9ytLmJgMP3i__68EPQpIN3vrPk&usqp=CAU'" class="h-full w-full bg-cover rounded-md" alt="garage">
+                          <img src="/uploads/${
+                            data.thumbnail
+                          }" onerror="this.src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeUyb754vebKqfbxScXd11wIOQGyxRlNNQBv31JG4wC9ytLmJgMP3i__68EPQpIN3vrPk&usqp=CAU'" class="h-full w-full bg-cover rounded-md" alt="garage">
                       </div>
                       <div class="garage-info w-3/4 p-2">
-                          <h3 class="font-medium text-[14px] text-left text-white wrap-none" id="garageName">${data.garage_name.length > 15
-          ? data.garage_name.substring(0, 12) + ".."
-          : data.garage_name
-        }</h3>
+                          <h3 class="font-medium text-[14px] text-left text-white wrap-none" id="garageName">${
+                            data.garage_name.length > 15
+                              ? data.garage_name.substring(0, 12) + ".."
+                              : data.garage_name
+                          }</h3>
                           <div class="flex pt-2">
                               <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" viewBox="0 0 24 24" style="fill:rgba(255,255,255,.6);"><path d="M11.42 21.815a1.004 1.004 0 0 0 1.16 0C12.884 21.598 20.029 16.44 20 10c0-4.411-3.589-8-8-8S4 5.589 4 9.996c-.029 6.444 7.116 11.602 7.42 11.819zM12 4c3.309 0 6 2.691 6 6.004.021 4.438-4.388 8.423-6 9.731-1.611-1.308-6.021-5.293-6-9.735 0-3.309 2.691-6 6-6z"></path><path d="M11 14h2v-3h3V9h-3V6h-2v3H8v2h3z"></path></svg>
                               <p class="pl-2 text-xs break-all text-[rgba(255,255,255,.6)] ">${address}</p>
@@ -250,8 +268,9 @@ class htmlHandler {
     let serviceList = "";
     serviceData.forEach((ele) => {
       serviceList += `
-                            <input type="checkbox" class="hidden" name="service" id="${ele.id
-        }" value="${ele.id}">
+                            <input type="checkbox" class="hidden" name="service" id="${
+                              ele.id
+                            }" value="${ele.id}">
                             <label for="${ele.id}" class="cursor-pointer">
                             <div class="relative bg-[rgba(0,0,0,.2)] p-2 w-full h-[100px] mt-5 rounded-lg flex" style="box-shadow:1px 1px 1px rgba(0,0,0,.2),inset 1px 1px 1px rgba(255,255,255,.2)">
                 <div class="garage-icon border-2 w-2/4 rounded-md overflow-hidden">
@@ -260,14 +279,15 @@ class htmlHandler {
                                         class="h-full w-full bg-cover" alt="garage" class="object-cover">
                 </div>
                 <div class="garage-info w-3/4 service-info pl-2">
-                                <h3 class="font-semibold text-[14px] text-left text-white wrap-none" id="serviceName">${ele.name.length > 15
-          ? ele.name.substring(0, 12) + ".."
-          : ele.name
-        }</h3>
+                                <h3 class="font-semibold text-[14px] text-left text-white wrap-none" id="serviceName">${
+                                  ele.name.length > 15
+                                    ? ele.name.substring(0, 12) + ".."
+                                    : ele.name
+                                }</h3>
                                 <p class=" text-xs text-left text-white">${ele.description.substring(
-          0,
-          40
-        )}..</p>
+                                  0,
+                                  40
+                                )}..</p>
                                 </div>
                                 <div class="flex justify-end items-center absolute bottom-2 right-2">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px"
@@ -275,8 +295,9 @@ class htmlHandler {
                                                 <path fill="white"
                                                         d="M17 6V4H6v2h3.5c1.302 0 2.401.838 2.815 2H6v2h6.315A2.994 2.994 0 0 1 9.5 12H6v2.414L11.586 20h2.828l-6-6H9.5a5.007 5.007 0 0 0 4.898-4H17V8h-2.602a4.933 4.933 0 0 0-.924-2z" />
                                         </svg>
-                                        <p class="text-white text-md text-bold">${ele.price
-        }</p>
+                                        <p class="text-white text-md text-bold">${
+                                          ele.price
+                                        }</p>
                                 </div>
                 </div>
                 </label>
@@ -288,12 +309,14 @@ class htmlHandler {
     let slotHTML = "";
     slotData.forEach((slot) => {
       slotHTML += `
-                            <input type='radio' id='slot-${slot.id
-        }' name='slots' value='${slot.id}'/>
+                            <input type='radio' id='slot-${
+                              slot.id
+                            }' name='slots' value='${slot.id}'/>
                             <label for="slot-${slot.id}" class="cursor-pointer">
                             <div class="flex justify-center items-center relative bg-[rgba(0,0,0,.2)] p-2 w-full h-[100px] mt-5 rounded-lg " style="box-shadow:1px 1px 1px rgba(0,0,0,.2),inset 1px 1px 1px rgba(255,255,255,.2)">
-                            <strong class="text-white text-bold text-2xl">${slot.start_time.split(" ")[1]
-        } - ${slot.end_time.split(" ")[1]}</strong>
+                            <strong class="text-white text-bold text-2xl">${
+                              slot.start_time.split(" ")[1]
+                            } - ${slot.end_time.split(" ")[1]}</strong>
                                 </div></label>
                             `;
     });
@@ -345,6 +368,7 @@ class steps {
   }
   static slots(date) {
     let id = localStorage.getItem("garageId");
+    console.log("called");
     date = date || new Date().toISOString().split("T")[0];
     htmlHandler.getData(
       "slots",
