7c3d1d1e6ebea3c84b9da64566afeb74273132d1
Mujpara-Nitin-Trainee <nitin.mujpara.2024@gmail.com>
2024-05-02 14:57:59 +0530

    Fix: solved bugs at clientside

--- utils/dbHandler.js
+++ utils/dbHandler.js
@@ -636,7 +636,7 @@ export const getNotifications = async (userId) => {
 export const userNotification = async (userId) => {
   try {
     let query =
-      "select c.id as id, d.name as customerName,  b.start_time as startTime, b.end_time as endTime from owner_has_garages as a join slot_master as b join appointments as c join users as d on a.garage_id = b.garage_id and b.id = c.slot_id and c.customer_id = d.id where d.id = ? and c.status = 2;";
+      "select c.id as id, d.name as customerName,  b.start_time as startTime, b.end_time as endTime from owner_has_garages as a join slot_master as b join appointments as c join users as d on a.garage_id = b.garage_id and b.id = c.slot_id and c.customer_id = d.id where d.id = ? and c.status = 2 or c.status = 4;";
 
     let result = await conn.query(query, userId);
     return result[0];
